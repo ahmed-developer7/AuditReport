4c73a45fe953fe790fdcff31386ca6a11009c0b7
ahmed-developer7 <ahmed.developer7@gmail.com>
2023-07-15 20:03:59 +0500

    initial

---
+++ HtmltoWordConverter/IDataInitializationService.cs
@@ -0,0 +1,17 @@
+﻿using HtmltoWordConverter.Models;
+using System.Collections.Generic;
+
+namespace HtmltoWordConverter
+{
+    public interface IDataInitializationService
+    {
+        List<Company> GetCompanies();
+        List<Client> getClients();
+        List<Department> getDepartments();
+        List<Observation1> getObservations();
+        List<Products> GetProducts();
+        List<SubDepartment> GetSubDepartments(int id);
+
+        //void InitializeData();
+    }
+}
